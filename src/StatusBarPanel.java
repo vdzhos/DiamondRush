98f64f95eeb7953100f1dcf9911421f57b68bad8
Sitkov Illia <illiasitkov@gmail.com>
2020-05-20 16:33:48 +0300

    initial values for game

--- src/StatusBarPanel.java
+++ src/StatusBarPanel.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 
 public class StatusBarPanel extends JPanel {
-    String n = null;
-    String m = null;
+
 }
--- src/Values.java
+++ src/Values.java
@@ -1,2 +1,22 @@
-public class Values {
+public interface Values {
+
+    //game panel (the one where all the actions are performed)
+    int GAME_PANEL_WIDTH = 700;
+    int GAME_PANEL_LENGTH = 700;
+    int GAME_PANEL_X = 0;
+    int GAME_PANEL_Y = 100;
+
+    //size of a cell on the map
+    int CELL_SIZE = 70;
+    int CELL_WIDTH = 70;
+    int CELL_LENGTH = 70;
+
+    //status bar with level, number of diamonds
+    int GAME_STATUS_BAR_X = 0;
+    int GAME_STATUS_BAR_Y = 0;
+    int GAME_STATUS_BAR_LENGTH = 100;
+    int GAME_STATUS_BAR_WIDTH = 700;
+
+
+
 }
