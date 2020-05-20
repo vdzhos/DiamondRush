bac16bc30f84f3ec4c2a29c950537a37bf2a1565
vdzhos <54538860+vdzhos@users.noreply.github.com>
2020-05-20 15:32:34 +0300

    Created a blank project

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Game extends JFrame {
+
+    public Game(){
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+}
