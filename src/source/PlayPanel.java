72de55329d4d996e8e89eb38128bc261e6faf26e
Sitkov Illia <illiasitkov@gmail.com>
2020-05-28 15:57:59 +0300

    Boys says some words when comes across the doors with keys or diamonds or checkpoints 2

--- src/source/PlayPanel.java
+++ src/source/PlayPanel.java
@@ -17,7 +17,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 public class PlayPanel extends JPanel implements KeyListener {
